92c609091fd154e3d87879f63728f43bf715021f
Chul Ahn <scahn.x@gmail.com>
2018-11-18 09:50:07 -0500

    initial commit

---
+++ ChatBot/Lab 3 Form Bot/SurveyForm.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using Microsoft.Bot.Builder.FormFlow;
+
+namespace Lab_3_Form_Bot
+{
+  public enum DepartmentOptions
+  {
+    Accounting,
+    AdministrativeSupport,
+    IT
+  }
+
+  [Serializable]
+  public class SurveyForm
+  {
+    [Prompt("Please enter your {&}.")]
+    public string Name;
+
+    [Prompt("Please enter your {&}.")]
+    public string PhoneNumber;
+
+    [Prompt("Please enter your {&}.")]
+    public string Address;
+
+    [Prompt("What {&} did it happen?")]
+    public string Date;
+
+    [Prompt("{&} did it happen?")]
+    public string How;
+
+    [Prompt("Any {&}?")]
+    public string Witnesses;
+
+    [Prompt("Do you have {&} (Pictures, Police report)?")]
+    public string Evidence;
+    
+    public static IForm<SurveyForm> BuildForm()
+    {
+      return new FormBuilder<SurveyForm>().Build();
+    }
+  }
+}
